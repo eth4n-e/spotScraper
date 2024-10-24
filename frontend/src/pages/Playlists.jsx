acc6b3d86dede03a3a2b8c6fd8bf1fea3c97d0c7
eth4n-e <etep9497@colorado.edu>
2024-10-24 15:08:50 -0600

    made playlist cards and made them clickable, need to add a UI for when cards are clicked

--- frontend/src/pages/Playlists.jsx
+++ frontend/src/pages/Playlists.jsx
@@ -1,4 +1,5 @@
 import NavBar from '../components/NavBar';
+import PlaylistCard from '../components/PlaylistCard';
 import { useEffect, useState } from 'react'
 import { useLoaderData } from 'react-router-dom'
 import axios from 'axios';
@@ -49,11 +50,7 @@ const Playlists = () => {
             <div className='mt-4 pb-4 mx-4 grid grid-cols-4 gap-6'>
                 {
                     playlists && (playlists.map( (playlist) => (
-                    <div onClick={() => handleCardClick(playlist.id)} className="rounded-md p-2 bg-beige2 shadow-inner shadow-brown2 transition ease-in-out duration-500 hover:-translate-y-1 hover:bg-beige1 hover:shadow-2xl hover:shadow-brown1 hover:border hover:border-brown1" key={playlist.id}>
-                        <img className="mx-auto w-11/12 h-20 sm:h-32 md:h-48 lg:h-64 xl:h-96 rounded-md drop-shadow-xl" src={playlist.images[0].url} alt="Playlist cover"/>
-                        <p className="mt-2 text-center text-brown3 font-semibold">{playlist.name}</p>
-                        <p className="mt-2 text-center text-brown3 font-semibold">{playlist.tracks.total} tracks</p>
-                    </div>  
+                        <PlaylistCard playlist={playlist} handleCardClick={handleCardClick} isClicked={clickedPlaylists.includes(playlist.id)}/>
                     )))
                 }
             </div>
